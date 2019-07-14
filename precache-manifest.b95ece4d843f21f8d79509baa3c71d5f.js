c025f443d0e74b1e96d50854d144fa4548605b7e
david <dhernandez@intersysconsulting.com>
2019-07-13 23:07:32 -0500

    Updates

---
+++ precache-manifest.b95ece4d843f21f8d79509baa3c71d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf56a4ff6be3ede335a19e4a69ca9dd",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "2b3c5721f604e74142ce",
+    "url": "/react-counter/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "b98cebf9a0b47fa9efd5",
+    "url": "/react-counter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2b3c5721f604e74142ce",
+    "url": "/react-counter/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "b98cebf9a0b47fa9efd5",
+    "url": "/react-counter/static/js/main.90f1225d.chunk.js"
+  },
+  {
+    "revision": "2c73fdf03faab0d3c96b",
+    "url": "/react-counter/static/js/runtime~main.35208266.js"
+  }
+]);
